22150c47967aa52fbc421d3b08285790195d37bf
收藏夹1区 <41465042+shoucangjia1qu@users.noreply.github.com>
2018-08-15 21:59:02 +0800

    重新了解了下

--- files_copy_collect.py
+++ files_copy_collect.py
@@ -7,9 +7,9 @@ Created on Wed Aug 15 15:24:08 2018
 
 import os, shutil
 
-
+print(os.getcwd())
 path = input("请输入需要整理的文件夹路径")
-os.chdir(path)
+#os.chdir(path)
 fileext = input("请输入需要整理的文件后缀，多个以英文逗号隔开（如：jpg,png,docx,xls等）").split(",")
 pathfiles = os.walk(path)
 
@@ -23,22 +23,18 @@ for dirname, subdir, files in pathfiles:
     if basename == SaveDir:
         continue
     #创建专属文件夹
-    if not os.path.exists(SaveDir):
-        os.mkdir(dirname+"\\"+SaveDir)
+    if not os.path.exists(path+"\\"+SaveDir):
+        os.mkdir(path+"\\"+SaveDir)
     #搜索找到图片文件->改文件名->复制
     
     for file in files:
         ext = file.split('.')[-1]
         if ext in exts:
             old_name = dirname+"\\"+file
-            new_name = SaveDir+"\\"+"Pic%d"%n+"."+ext
+            new_name = path+"\\"+SaveDir+"\\"+"Pic%d"%n+"."+ext
             shutil.copy(old_name, new_name)
             print(old_name)
             n+=1
-
-
-
-
 
 
 
